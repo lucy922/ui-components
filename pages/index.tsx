52686aeef331fa9a9128fbb0a8b82b18620bf69f
lucy922 <ofemlucy@gmail.com>
2022-11-22 15:02:22 +0100

    create card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,29 @@
 import type { NextPage } from "next";
 import { Button } from "../components/Button";
+import { Card } from "../components/Card";
 import { Input } from "../components/Input";
 import { Label } from "../components/Label";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col p-4">
-      <Label>Email</Label>
-      <Input
-        type="text"
-        placeholder="Enter Email..."
-        size="md"
-        variant="filled"
-      />
-      <Button type="submit" size="sm" variant="primary">
-        Button
-      </Button>
+    <div className="flex flex-col mt-7 items-center">
+      <Card bgColor="gray">
+        <Label>Email</Label>
+        <Input
+          type="text"
+          placeholder="Enter Email..."
+          size="md"
+          variant="filled"
+        />
+        <Button type="submit" size="sm" variant="primary">
+          Button
+        </Button>
+        <div>ndfyuefguwh</div>
+        <div>ndfyuefguwh</div>
+        <div>ndfyuefguwh</div>
+        <div>ndfyuefguwh</div>
+        <div>ndfyuefguwh</div>
+      </Card>
     </div>
   );
 };
